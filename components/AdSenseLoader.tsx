684a0957bf134582c4b1a79f5906cf1b575877a1
Harry <harrypb007@gmail.com>
2026-04-21 18:33:32 -1200

    Use Head script for AdSense loader

--- components/AdSenseLoader.tsx
+++ components/AdSenseLoader.tsx
@@ -1,16 +1,18 @@
-import Script from 'next/script';
+import Head from "next/head";
 
 export default function AdSenseLoader() {
   const client = process.env.NEXT_PUBLIC_ADSENSE_CLIENT;
+
   if (!client) return null;
 
   return (
-    <Script
-      id="adsense-script"
-      
-      strategy="afterInteractive"
-      src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${client}`}
-      crossOrigin="anonymous"
-    />
+    <Head>
+      <script
+        id="adsense-script"
+        async
+        src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${client}`}
+        crossOrigin="anonymous"
+      />
+    </Head>
   );
 }
