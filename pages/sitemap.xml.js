8b0b2214bcc89f3f49ef380e4b40e3cc3a504d98
Harry <harrypb007@gmail.com>
2026-04-24 19:06:29 -1200

    Fix sitemap properly

---
+++ pages/sitemap.xml.js
@@ -0,0 +1,28 @@
+export default function handler(req, res) {
+  const baseUrl = 'https://calicalculators.com';
+
+  const pages = [
+    '',
+    '/bmi-calculator',
+    '/age-calculator',
+    '/percentage-calculator',
+  ];
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${pages
+      .map((page) => {
+        return `
+          <url>
+            <loc>${baseUrl}${page}</loc>
+            <lastmod>${new Date().toISOString()}</lastmod>
+          </url>
+        `;
+      })
+      .join('')}
+  </urlset>`;
+
+  res.setHeader('Content-Type', 'text/xml');
+  res.write(sitemap);
+  res.end();
+}
