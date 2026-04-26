32f6a740daa148a5237e2f216fa5dcd0dd4af832
Harry <harrypb007@gmail.com>
2026-04-25 17:53:55 -1200

    add favicon and seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import '../styles/globals.css'
+import './styles/globals.css'
 import Head from 'next/head'
 import Navbar from '../components/Navbar'
 import SiteFooter from '../components/SiteFooter'
@@ -7,13 +7,20 @@ export default function App({ Component, pageProps }: any) {
   return (
     <>
       <Head>
-        {/* Google AdSense */}
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3011404436008275"
-          crossOrigin="anonymous"
-        ></script>
-      </Head>
+  {/* Favicon */}
+  <link rel="icon" href="/favicon.ico" />
+
+  {/* Title + SEO */}
+  <title>CaliCalculators | Premium Online Calculators</title>
+  <meta name="description" content="Free online calculators for finance, health, math and more." />
+
+  {/* Google AdSense */}
+  <script
+    async
+    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3011404436008275"
+    crossOrigin="anonymous"
+  ></script>
+</Head>
 
       <Component {...pageProps} />
 
